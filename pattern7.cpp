a87d2ae418761ac984c64d68b75b606210d4dcc9
2007aman <palrajni073@gmail.com>
2026-03-17 20:34:27 +0530

    added the pattern 7

---
+++ pattern7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+void printn(int n) {
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == 0||j == 0|| i ==n - 1 || j ==n - 1){
+       std::cout << "*";
+      } else {
+       std::cout <<std::endl;
+      }
+    }
+  }
+}
+int main() {
+  int t;
+  std:: cin >> t;
+  for (int i = 0; i < t; i++) {
+  int n;
+  std::cin >> n;
+  printn(n);
+  }
+}
