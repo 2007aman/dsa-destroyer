7f1fad1a18efbf69c20a7509ef4c8936184fbf14
2007aman <palrajni073@gmail.com>
2026-07-17 11:11:52 +0530

    dsa

--- moving_negative.cpp
+++ moving_negative.cpp
@@ -4,14 +4,21 @@ int j=0;
 for(int i=0;i<arr_size;i++){
 if(arr[i]<j){
 std::cout<<"found negative number"<<std::endl;
+std::swap(arr[i],j);
 }
 else{
 std::cout<<"found positive number"<<std::endl;
 }
+
 }
+
 }
 int main(){
 int arr_size=6;
 int arr[6]={1,3,-3,3,-2,-23};
-move(4,arr);
+move(6,arr);
+for(int i=0;i<arr_size;i++){
+std::cout<<arr[i]<<" ";
+}
+
 }
